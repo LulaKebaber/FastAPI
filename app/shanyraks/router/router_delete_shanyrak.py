8f186d255075a1c3111b236f926acdedd3b575e0
Danial <danial-baizak@mail.ru>
2023-06-13 21:34:43 +0600

    task 6 last bug fixed

--- app/shanyraks/router/router_delete_shanyrak.py
+++ app/shanyraks/router/router_delete_shanyrak.py
@@ -10,13 +10,13 @@ from ..service import Service, get_service
 from . import router
 
 
-@router.patch("/{shanyrak_id:str}")
+@router.delete("/{shanyrak_id:str}")
 def delete_shanyrak(
     shanyrak_id: str,
     jwt_data: JWTData = Depends(parse_jwt_user_data),
     svc: Service = Depends(get_service),
 ) -> dict[str, str]:
     shanyrak_deleted = svc.repository.delete_shanyrak(shanyrak_id, jwt_data.user_id)
-    if shanyrak_deleted.modified_count == 1:
+    if shanyrak_deleted.deleted_count == 1:
         return Response(status_code=200)
     return Response(status_code=404)
