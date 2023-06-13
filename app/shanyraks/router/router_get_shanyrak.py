10bfaf0b919d7cf304fb1684981788dd642cb041
Danial <danial-baizak@mail.ru>
2023-06-13 18:47:40 +0600

    task 4

---
+++ app/shanyraks/router/router_get_shanyrak.py
@@ -0,0 +1,34 @@
+from typing import Any
+
+from fastapi import Depends, Response
+from pydantic import Field
+
+from app.utils import AppModel
+
+from ..adapters.jwt_service import JWTData
+from ..service import Service, get_service
+from . import router
+from .dependencies import parse_jwt_user_data
+
+
+class GetShanyrakResponse(AppModel):
+    id: Any = Field(alias="_id")
+    type: str
+    price: int
+    address: str
+    area: float
+    rooms_count: int
+    description: str
+    user_id: str
+
+
+@router.get("/{shanyrak_id:str}", response_model=GetShanyrakResponse)
+def get_shanyrak(
+    shanyrak_id: str,
+    jwt_data: JWTData = Depends(parse_jwt_user_data),
+    svc: Service = Depends(get_service),
+) -> dict[str, str]:
+    shanyrak = svc.repository.get_shanyrak(shanyrak_id)
+    if shanyrak is None:
+        return Response(status_code=404)
+    return GetShanyrakResponse(**shanyrak)
